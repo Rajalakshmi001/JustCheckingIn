1303d475b7ca108267add33c9c6206f75b5ab338
Raji <rajinisha1994@gmail.com>
2018-05-15 16:44:37 -0400

    [:pair programming] dynamic notifications for reminders done. Removed unused imports

--- app/src/main/java/edu/rosehulman/scottae/justcheckingin/fragments/ReminderFragment.java
+++ app/src/main/java/edu/rosehulman/scottae/justcheckingin/fragments/ReminderFragment.java
@@ -1,21 +1,13 @@
 package edu.rosehulman.scottae.justcheckingin.fragments;
 
-import android.app.AlarmManager;
 import android.app.AlertDialog;
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.DialogInterface;
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.SystemClock;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.text.InputType;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -29,12 +21,9 @@ import java.util.Calendar;
 import java.util.Date;
 
 import edu.rosehulman.scottae.justcheckingin.R;
-import edu.rosehulman.scottae.justcheckingin.activities.DisplayReminderNotification;
 import edu.rosehulman.scottae.justcheckingin.activities.MainActivity;
 import edu.rosehulman.scottae.justcheckingin.adapters.ReminderListAdapter;
 import edu.rosehulman.scottae.justcheckingin.models.Reminder;
-import edu.rosehulman.scottae.justcheckingin.utils.Constants;
-import edu.rosehulman.scottae.justcheckingin.utils.NotificationBroadcastReceiver;
 
 /**
  * A placeholder fragment containing a simple view.
